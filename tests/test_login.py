6d81a2c3529d54244c83c2098bdfd4a47a2ae69c
colleague <yuzhakovadasha@gmail.com>
2025-07-24 14:49:59 +0400

    login update

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,7 +2,7 @@ def test_login_page_load():
     print("login page load successful")
 
 def test_successful_login():
-    print("successful login")
+    print("successful login + successful login")
 
 def test_log_out():
     print("logout successful")
