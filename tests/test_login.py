92d2d705199e90de90a846fce3c2ec2c995a1c1c
darya_yuzh <42751581+dariayuzh@users.noreply.github.com>
2025-07-24 12:11:21 +0400

    [QA-001] Update tests for login (#1)

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,7 @@ def test_successful_login():
     print("successful login")
 
 def test_log_out():
-    print("log out")
+    print("logout successful")
+
     a = 1
     assert a == 1, f"{a} does have expected value = 1"
