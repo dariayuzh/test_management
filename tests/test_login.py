85e70caab8717464eb22505d61100c60bed17907
dyuzhakova <yuzhakovadasha@gmail.com>
2025-08-15 18:42:03 +0400

    Add test for login page loads

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,13 @@
 def test_successful_login():
     a = 1
     assert a == 1, f"{a} is not equal 1!"
-    print("successful login")
+    print("User is logged in successfully!")
 
 def test_login_error():
     print("Error during login.")
+
+def test_login_page_loads():
+    print("Login page is load!")
 
 def test_logout():
     print("logout!")
