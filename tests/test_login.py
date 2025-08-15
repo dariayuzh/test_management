fb7b8420325856a0ad0eb9bc9521275a11927b2f
dariayuzh <yuzhakovadasha@gmail.com>
2025-08-15 18:38:39 +0400

    Add test for logout

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,10 @@
 def test_successful_login():
+    a = 1
+    assert a == 1, f"{a} is not equal 1!"
     print("successful login")
 
 def test_login_error():
     print("Error during login.")
+
+def test_logout():
+    print("logout!")
